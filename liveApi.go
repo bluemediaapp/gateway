7976764d1379d0970146e348e728cb19e80d8adb
Tag-Epic <tagepicuwu@gmail.com>
2021-04-13 15:40:32 +0200

    Feat: Add recommended route

--- liveApi.go
+++ liveApi.go
@@ -1,5 +1,13 @@
 package main
 
-func initLiveApi()  {
+import "github.com/gofiber/fiber/v2"
 
+func initLiveApi() {
+	app.Get("/api/live/recommended", func(ctx *fiber.Ctx) error {
+		userId := ctx.Get("user-id", "")
+		if userId == "" {
+			return ctx.Status(403).SendString("Invalid login token")
+		}
+		return forwardHttp(ctx, "http://recommended:5000/user/"+userId)
+	})
 }
